fda4de7e2ae4c89e71b39c18ed29e44b9874b1e8
tltlla <104186449+tltlla@users.noreply.github.com>
2022-08-08 15:09:33 +0900

    Add files via upload

---
+++ pratice-githubAction-deploy/src/test/java/com/codestates/praticegithubActiondeploy/PraticeGithubActionDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.praticegithubActiondeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticeGithubActionDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
